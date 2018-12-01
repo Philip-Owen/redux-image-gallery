99ed8aa02daeba471d195e3494b0841f06b5f139
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-12-01 15:10:18 -0600

    configured local state for upload and app components. configured upload component to pass urls to app state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import Gallery from './Gallery';
 import Upload from './Upload';
 
 export default class App extends Component {
+	state = {
+		images: [],
+  };
+  addImage = (imageUrl) => {
+    const images = this.state.images;
+    this.setState({images: [...images, imageUrl]});
+  }
 	render() {
 		return (
-      <div>
-        <Upload />
-        <Gallery />
+			<div>
+				<Upload addImage={this.addImage}/>
+				<Gallery />
 			</div>
 		);
 	}
--- src/Upload.js
+++ src/Upload.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Upload extends Component {
-  render() {
-    return (
-      <div>
-        <form>
-          <input type="text" placeholder="Image URL"/>
-          <button>Add to Gallery</button>
-        </form>
-      </div>
-    )
-  }
+	state = {
+		imageUrl: '',
+	};
+	handleChange = e => {
+		const imageUrl = e.target.value;
+		this.setState({ imageUrl });
+	};
+	handleSubmit = e => {
+		e.preventDefault();
+		this.props.addImage(this.state.imageUrl);
+		e.target.reset();
+		this.setState({ imageUrl: '' });
+	};
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<input type="text" placeholder="Image URL" onChange={this.handleChange} />
+					<button>Add to Gallery</button>
+				</form>
+			</div>
+		);
+	}
 }

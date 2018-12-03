5ee2ef3c85cd818417ea4e78cc93cd37855e39fd
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-12-02 18:25:22 -0600

    added base redux configuration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 import Gallery from './Gallery';
 import Upload from './Upload';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const initialState = {
+	images: [],
+};
+
+const reducer = function(state = initialState, action) {
+	const { type } = action;
+	switch (type) {
+		case 'ADD_IMAGE':
+			console.log('From ADD_IMAGE reducer');
+			break;
+		default:
+			return state;
+	}
+};
+
+const rootReducer = combineReducers({
+	reducer,
+});
+
+const store = createStore(rootReducer, {}, composeWithDevTools());
 
 export default class App extends Component {
 	state = {
 		images: [],
-  };
-  addImage = (imageUrl) => {
-    const images = this.state.images;
-    this.setState({images: [...images, imageUrl]});
-  }
+	};
+	addImage = imageUrl => {
+		const images = this.state.images;
+		this.setState({ images: [...images, imageUrl] });
+	};
 	render() {
 		return (
-			<div>
-				<Upload addImage={this.addImage}/>
-				<Gallery images={this.state.images}/>
-			</div>
+			<Provider store={store}>
+				<div>
+					<Upload addImage={this.addImage} />
+					<Gallery images={this.state.images} />
+				</div>	
+			</Provider>
 		);
 	}
 }

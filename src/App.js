e8d4063a4438448e561a07a2445f109797086c9f
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-12-05 20:13:05 -0600

    refactored app component to stateless component and added additional styling

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import styled from 'styled-components';
 import rootReducer from './rootReducer';
 import Gallery from './Gallery';
 import Upload from './Upload';
 
 const store = createStore(rootReducer, {}, composeWithDevTools());
 
-export default class App extends Component {
-	render() {
-		return (
-			<Provider store={store}>
-				<div>
-					<Upload />
-					<Gallery />
-				</div>
-			</Provider>
-		);
-	}
-}
+const App = () => (
+	<Provider store={store}>
+		<AppContainer>
+			<Upload />
+			<Gallery />
+		</AppContainer>
+	</Provider>
+);
+
+const AppContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+`;
+
+export default App;
